3714d39a65b40cc711438daf9b3e804ac6f8b533
Jan Knedlik <J.Knedlik@gsi.de>
2016-01-22 09:28:07 +0100

    changed the test: main.cpp to use the new XrdCl  Client, changed makefile to better use everything else

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include "XrdRedirLoc.cc"
-int main(){
-  std::map<std::string,std::string> x;
-  std::cout<<"stuff1"<<std::endl;
-  XrdRedirectToLocal::RedLocalFactory fac(x);
-  std::cout<<"stuff2"<<std::endl;
-  XrdCl::FilePlugIn * fp=fac.CreateFile("root://xrd-server///home/xrduser/stuff");
-  XrdCl::OpenFlags::Flags f;
-  Access::Mode mode;
-const char * test="teststuff";
+int main(int argc, char** argv){
 
-  fp->Open("root://xrd-server///xrdmount/data/i", f , mode , new XrdCl::ResponseHandler,167);
-  fp->Write(13,9,(void *)test,new XrdCl::ResponseHandler,166);
-  fp->Close(new XrdCl::ResponseHandler,166);
-  char wait;
-cin>>wait;
+  std::map<std::string,std::string> x;
+  XrdCl::File * fp=new XrdCl::File(true);
+  XrdCl::OpenFlags::Flags f;
+  f=XrdCl::OpenFlags::Write;
+  Access::Mode mode;
+  const char * test="i have been written from the local client";
+  XrdCl::ResponseHandler* handler=new XrdCl::ResponseHandler;
+  fp->Open(argv[1], f , mode ,handler,0);
+  fp->Write(13,9,(void *)test,handler,0);
+  fp->Close(handler,0);
 }
--- makefile
+++ makefile
@@ -3,8 +3,8 @@
 all:redir
 
 test: clean
-	g++ -I/xr4i/myplug/src -c *.cpp ./src/*.cc 
-	g++  -o test.exe *.o -L/xr4i/lib -lpthread -lXrdUtils -lXrdCl -lXrdPosix -lXrdFfs
+	g++ -I/xr4i/myplug/src -I./src -c *.cpp ./src/*.cc 
+	g++  -o test.exe *.o -L/lustre/nyx/rz/jknedlik/xrootd_install/lib -lpthread -lXrdUtils -lXrdCl -lXrdPosix -lXrdFfs
 redir: clean
 	g++ -fPIC  -I ./src/ -c *.cc -std=c++11
 	g++ -shared -Wl,-soname,XrdRedirLoc.so.1 -o XrdRedir.Loc.so.1.0 *.o
