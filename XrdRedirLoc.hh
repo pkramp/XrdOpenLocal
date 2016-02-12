6f8dd2da4050e42ae9a11a0006d9d674df51a9a7
Jan Knedlik <J.Knedlik@gsi.de>
2016-02-12 15:30:50 +0100

    intended c++ files

--- XrdRedirLoc.hh
+++ XrdRedirLoc.hh
@@ -1,5 +1,5 @@
-#ifndef __XRDREDIRCT_TOLOCAL_PLUGIN_HH___ 
-#define __XRDREDIRCT_TOLOCAL_PLUGIN_HH___ 
+#ifndef __XRDREDIRCT_TOLOCAL_PLUGIN_HH___
+#define __XRDREDIRCT_TOLOCAL_PLUGIN_HH___
 #include <stdio.h>
 #include "XrdCl/XrdClPlugInInterface.hh"
 #include <algorithm>
@@ -16,7 +16,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <stdexcept> 
+#include <stdexcept>
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -24,36 +24,36 @@
 extern XrdCl::Log XrdClRFSLog;
 
 
-namespace XrdRedirectToLocal
-{
-  //----------------------------------------------------------------------------
-  // Plugin factory
-  //----------------------------------------------------------------------------
-  class RedLocalFactory: public XrdCl::PlugInFactory
-  {
-    public:
-      //------------------------------------------------------------------------
-      // Constructore
-      //------------------------------------------------------------------------
-     RedLocalFactory(const std::map<std::string, std::string> &config);
-      //------------------------------------------------------------------------
-      // Destructor
-     void loadDefaultConf(std::map<std::string,std::string>& config);
-  //------------------------------------------------------------------------
-     virtual ~RedLocalFactory() {}
+namespace XrdRedirectToLocal {
+//----------------------------------------------------------------------------
+// Plugin factory
+//----------------------------------------------------------------------------
+class RedLocalFactory: public XrdCl::PlugInFactory {
+	public:
+		//------------------------------------------------------------------------
+		// Constructore
+		//------------------------------------------------------------------------
+		RedLocalFactory(const std::map<std::string, std::string> &config);
+		//------------------------------------------------------------------------
+		// Destructor
+		virtual ~RedLocalFactory() {}
 
-      //------------------------------------------------------------------------
-      // Create a file plug-in for the given URL
-      //------------------------------------------------------------------------
-      virtual XrdCl::FilePlugIn *CreateFile( const std::string &url );
+		//------------------------------------------------------------------------
+		// Create a file plug-in for the given URL
+		//------------------------------------------------------------------------
+		virtual XrdCl::FilePlugIn *CreateFile( const std::string &url );
 
-      //------------------------------------------------------------------------
-      // Create a file system plug-in for the give/Lon URL
-    virtual XrdCl::FileSystemPlugIn *CreateFileSystem(const std::string &url);
-      //------------------------------------------------------------------------
-    
-  private:
-  };
+		//------------------------------------------------------------------------
+		// Create a file system plug-in for the give/Lon URL
+		virtual XrdCl::FileSystemPlugIn *CreateFileSystem(const std::string &url);
+		//------------------------------------------------------------------------
+		// load default config:
+		void loadDefaultConf(std::map<std::string,std::string>& config);
+		//------------------------------------------------------------------------
+		
+
+	private:
+};
 };
 
 #endif // __XRDREDIRCT_TOLOCAL_PLUGIN_HH___
