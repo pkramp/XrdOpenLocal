10d0cdcd575967eb84c20a66cffc24a9731431c7
Jan Knedlik <J.Knedlik@gsi.de>
2016-04-28 12:06:14 +0200

    The xrdcp test does now work independently

--- 0.1/test/xrdcp.test.sh
+++ 0.1/test/xrdcp.test.sh
@@ -4,10 +4,21 @@ if
     export XRD_LOGLEVEL=Dump
 
 fi
+
+###Setup the test
+cat > test/xrdopenlocal.conf << EOF
+lib = $PWD/XrdRedir.Loc.so
+redirectlocal = test.test|/tmp/
+enable = true
+EOF
+
 export XRD_PLUGIN=$PWD/XrdRedir.Loc.so
-export XrdRedirLocDEFAULTCONF=$PWD/client.plugins.d/xrdopenlocal.conf
+export XrdRedirLocDEFAULTCONF=$PWD/test/xrdopenlocal.conf
 mkdir /tmp/xrdcptest
 echo "test123" > /tmp/xrdcptest/testfile
+
+
+##Run the test
 xrdcp /tmp/xrdcptest/testfile .
 xrdcp root://test.test//xrdcptest/testfile ./testfile2
 DIFF=$(diff testfile testfile2)
@@ -16,7 +27,7 @@ if [ "$DIFF" == "" ]; then
 
 else
 
-    echo -e "\e[91m SUCCESS \e[0m"
+    echo -e "\e[91m FAILED \e[0m"
 fi
-rm testfile testfile2
-rm -r /tmp/xrdcptest
+###Cleanup the test
+rm -r testfile testfile2 /tmp/xrdcptest test/xrdopenlocal.conf
