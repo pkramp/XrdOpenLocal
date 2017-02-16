e7ca7575646c65b8a1b99553b0ebbc8e4cc9911e
Jan <j.knedlik@gsi.de>
2017-02-16 13:23:18 +0100

    vx

--- XrdOpenLocal.cc
+++ XrdOpenLocal.cc
@@ -28,7 +28,8 @@ private:
 	///@proxyPrefix The prefix that will be added to any root query that cannot use local available files
 	///@file file for local access
 	fstream* file;
-std::string path;
+	XrdCl::File xfile;
+	std::string path;
 public:
 	static void printInfo() {
 		XrdCl::Log *log= XrdCl::DefaultEnv::GetLog();
@@ -88,14 +89,12 @@ public:
 	}
 
 	//Constructor
-	OpenLocalFile(std::string x){ //declare that xfile shall not recursively use plugins
-		file=new fstream();
-
+	OpenLocalFile(std::string x):xfile(false){ //declare that xfile shall not recursively use plugins
 	}
 
 	//Destructor
 	~OpenLocalFile() {
-		file->close();
+		xfile.Close();
 	
 	}
 	//Open()
@@ -105,80 +104,32 @@ public:
 	                           ResponseHandler   *handler,
 	                           uint16_t           timeout ) {
 		XrdCl::Log *log=XrdCl::DefaultEnv::GetLog();
-		XRootDStatus* ret_st;
-		std::string newurl= LocalPath(url);
 		log->Debug(1,"OpenLocalFile::Open");
-			file->open(newurl.c_str(),std::ios::in  | std::ios::out| std::ios::app );
-			if(file->fail()) {
-				ret_st=new XRootDStatus( XrdCl::stError,XrdCl::errOSError, 1,"file could not be opened");
-				handler->HandleResponse(ret_st,0);
-				return *ret_st;
-			}
-			ret_st=new XRootDStatus(XrdCl::stOK,0,0,"");
-			handler->HandleResponse(ret_st,0);
-			return  *ret_st;
+		return xfile.Open(LocalPath(url),flags,mode,handler,timeout);
 		}
 
 	virtual XRootDStatus Close(ResponseHandler *handler,uint16_t timeout) {
 		XrdCl::Log *log=XrdCl::DefaultEnv::GetLog();
-			file->close();
-			XRootDStatus* ret_st=new XRootDStatus(XrdCl::stOK,0,0,"");
-			handler->HandleResponse(ret_st,0);
+			xfile.Close(handler,timeout);
 			return  XRootDStatus(XrdCl::stOK,0,0,"");
 
 
 	}
 
 	virtual bool IsOpen()  const    {
-	 return file->is_open();
+	 return xfile.IsOpen();
 	}
 	virtual XRootDStatus Stat(bool force,ResponseHandler *handler,uint16_t timeout) {
 		XrdCl::Log *log=XrdCl::DefaultEnv::GetLog();
 		log->Debug(1,"OpenLocalFile::Stat");
-
-			if(file->is_open() && file!=NULL) {
-				struct stat s;
-				stat(path.c_str(),&s);
-				StatInfo* sinfo = new StatInfo();
-				std::ostringstream data;
-				data<<s.st_dev <<" "<< s.st_size <<" "<<s.st_mode<<" "<<s.st_mtime ;
-				std::string output ="OpenLocal::Stat, stats are: (_dev,_size,_mode,_mtime) ";
-				output.append(data.str().c_str());
-				log->Debug(1,output.c_str());
-
-				if(!sinfo->ParseServerResponse(data.str().c_str())) {
-					delete sinfo;
-					return XRootDStatus(XrdCl::stError, errDataError);
-				} else {
-					XRootDStatus* ret_st = new XRootDStatus(XrdCl::stOK, 0, 0, "");
-					AnyObject* obj = new AnyObject();
-					obj->Set(sinfo);
-					handler->HandleResponse(ret_st, obj);
-					log->Debug( 1, "OpenLocalFile::Stat returning stat structure");
-					return XRootDStatus( XrdCl::stOK,0,0,"");
-				}
-
-
-			} else {
-				log->Debug(1,"OpenLocalFile::Stat::Error No file opened");
-				return XRootDStatus( XrdCl::stError,XrdCl::errOSError,-1,"no file opened error");
-			}
-				return XRootDStatus( XrdCl::stError,XrdCl::errOSError,-1,"no file opened error");
+	
+	return xfile.Stat(force,handler,timeout);
 	}
 
 	virtual XRootDStatus Read(uint64_t offset,uint32_t length,
 	                          void  *buffer,XrdCl::ResponseHandler *handler,
 	                          uint16_t timeout ) {
-		XrdCl::Log *log=XrdCl::DefaultEnv::GetLog();
-		log->Debug(1,"OpenLocal::Read");
-			file->seekp(offset);
-			file->read( (char*)buffer,length);
-			XRootDStatus* ret_st=new XRootDStatus(XrdCl::stOK,0,0,"");
-			ChunkInfo* chunkInfo=new ChunkInfo(offset,length,buffer );
-			AnyObject* obj=new AnyObject();
-			obj->Set(chunkInfo);
-			handler->HandleResponse(ret_st,obj);
-			return  XRootDStatus(XrdCl::stOK,0,0,"");
+		xfile.Read(offset,length,buffer,handler,timeout);
 	}
 
 	XRootDStatus Write( uint64_t         offset,
@@ -186,14 +137,7 @@ public:
 	                    const void      *buffer,
 	                    ResponseHandler *handler,
 	                    uint16_t         timeout = 0 ) {
-		XrdCl::Log *log=XrdCl::DefaultEnv::GetLog();
-		log->Debug(1,"OpenLocalFile::Write");
-		file->seekg(offset);
-		file->write((char*)buffer,size);
-		XRootDStatus* ret_st=new XRootDStatus(XrdCl::stOK,0,0,"");
-		handler->HandleResponse(ret_st,0);
-		return  XRootDStatus(XrdCl::stOK,0,0,"");
-
+	return xfile.Write(offset,size,buffer,handler,timeout);
 	}
 };
 std::map<std::string,std::string> OpenLocalFile::swapLocalMap ;
